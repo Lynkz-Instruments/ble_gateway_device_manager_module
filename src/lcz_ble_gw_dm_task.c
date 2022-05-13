a376b40731e4441dd99b3336fffbbef88860ae25
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-05-13 11:34:57 -0500

    Add LwM2M client
    Update state machine to connect to DM server with LwM2M.
    
    bug 21644

--- src/lcz_ble_gw_dm_task.c
+++ src/lcz_ble_gw_dm_task.c
@@ -16,7 +16,7 @@ LOG_MODULE_REGISTER(lcz_ble_gw_dm, CONFIG_LCZ_BLE_GW_DM_LOG_LEVEL);
 
 #include "fwk_includes.h"
 #include "lcz_network_monitor.h"
-
+#include "lcz_lwm2m_client.h"
 #include "lcz_ble_gw_dm_task.h"
 
 /**************************************************************************************************/
@@ -28,6 +28,8 @@ enum gw_dm_state {
 	GW_DM_STATE_WAIT_FOR_NETWORK = 0,
 	GW_DM_STATE_WAIT_BEFORE_DM_CONNECTION,
 	GW_DM_STATE_CONNECT_TO_DM,
+	GW_DM_STATE_WAIT_FOR_CONNECTION,
+	GW_DM_STATE_IDLE,
 	GW_DM_STATE_DISCONNECT_DM,
 };
 
@@ -36,6 +38,10 @@ typedef struct gw_dm_task_obj {
 	enum gw_dm_state state;
 	uint32_t timer;
 	bool network_ready;
+	uint32_t dm_connection_delay_seconds;
+	uint32_t dm_connection_timeout_seconds;
+	bool lwm2m_connection_err;
+	bool lwm2m_connected;
 } gw_dm_task_obj_t;
 
 /**************************************************************************************************/
@@ -48,6 +54,7 @@ static void gw_dm_fsm(void);
 static bool timer_expired(void);
 static void set_state(enum gw_dm_state next_state);
 static char *state_to_string(enum gw_dm_state state);
+static void lwm2m_client_connected_event(bool connected, enum lwm2m_rd_client_event client_event);
 
 /**************************************************************************************************/
 /* Local Data Definitions                                                                         */
@@ -55,7 +62,7 @@ static char *state_to_string(enum gw_dm_state state);
 static struct lcz_nm_event_agent event_agent;
 static gw_dm_task_obj_t gwto;
 K_MSGQ_DEFINE(gw_dm_task_queue, FWK_QUEUE_ENTRY_SIZE, GW_DM_TASK_QUEUE_DEPTH, FWK_QUEUE_ALIGNMENT);
-static uint32_t dm_connection_delay_seconds;
+static struct lcz_lwm2m_client_event_callback_agent lwm2m_event_agent;
 
 /**************************************************************************************************/
 /* Local Function Definitions                                                                     */
@@ -70,6 +77,10 @@ static char *state_to_string(enum gw_dm_state state)
 		return "Delay before DM connection";
 	case GW_DM_STATE_CONNECT_TO_DM:
 		return "Connect to DM";
+	case GW_DM_STATE_WAIT_FOR_CONNECTION:
+		return "Wait for connection";
+	case GW_DM_STATE_IDLE:
+		return "Idle";
 	case GW_DM_STATE_DISCONNECT_DM:
 		return "Disconnect DM";
 	default:
@@ -100,6 +111,8 @@ static bool timer_expired(void)
 
 static void gw_dm_fsm(void)
 {
+	int ret;
+
 	switch (gwto.state) {
 	case GW_DM_STATE_WAIT_FOR_NETWORK:
 		gwto.network_ready = lcz_nm_network_ready();
@@ -107,7 +120,7 @@ static void gw_dm_fsm(void)
 			FRAMEWORK_MSG_CREATE_AND_BROADCAST(FWK_ID_BLE_GW_DM, FMC_NETWORK_CONNECTED);
 			set_state(GW_DM_STATE_WAIT_BEFORE_DM_CONNECTION);
 		} else {
-			gwto.timer = dm_connection_delay_seconds;
+			gwto.timer = gwto.dm_connection_delay_seconds;
 		}
 		break;
 	case GW_DM_STATE_WAIT_BEFORE_DM_CONNECTION:
@@ -123,10 +136,41 @@ static void gw_dm_fsm(void)
 		if (!gwto.network_ready) {
 			set_state(GW_DM_STATE_WAIT_FOR_NETWORK);
 		} else {
-			/* connect to DM server */
+			gwto.lwm2m_connection_err = false;
+			gwto.timer = gwto.dm_connection_timeout_seconds;
+			ret = lcz_lwm2m_client_connect(CONFIG_LCZ_LWM2M_CLIENT_ENDPOINT_NAME,
+						       LCZ_LWM2M_CLIENT_TRANSPORT_UDP);
+			if (ret < 0) {
+				set_state(GW_DM_STATE_WAIT_FOR_NETWORK);
+			} else {
+				set_state(GW_DM_STATE_WAIT_FOR_CONNECTION);
+			}
+		}
+		break;
+	case GW_DM_STATE_WAIT_FOR_CONNECTION:
+		if (!gwto.network_ready) {
+			set_state(GW_DM_STATE_DISCONNECT_DM);
+		} else if (gwto.lwm2m_connection_err) {
+			set_state(GW_DM_STATE_WAIT_FOR_NETWORK);
+		} else {
+			if (gwto.lwm2m_connected) {
+				set_state(GW_DM_STATE_IDLE);
+			} else if (timer_expired()) {
+				set_state(GW_DM_STATE_DISCONNECT_DM);
+			}
+		}
+		break;
+	case GW_DM_STATE_IDLE:
+		if (!gwto.network_ready || !gwto.lwm2m_connected) {
+			set_state(GW_DM_STATE_DISCONNECT_DM);
 		}
 		break;
 	case GW_DM_STATE_DISCONNECT_DM:
+		if (!gwto.network_ready) {
+			lcz_lwm2m_client_disconnect(false);
+		} else {
+			lcz_lwm2m_client_disconnect(true);
+		}
 		set_state(GW_DM_STATE_WAIT_FOR_NETWORK);
 		break;
 	default:
@@ -165,6 +209,21 @@ static void nm_event_callback(enum lcz_nm_event event)
 	}
 }
 
+static void lwm2m_client_connected_event(bool connected, enum lwm2m_rd_client_event client_event)
+{
+	gwto.lwm2m_connected = connected;
+	switch (client_event) {
+	case LWM2M_RD_CLIENT_EVENT_BOOTSTRAP_REG_FAILURE:
+	case LWM2M_RD_CLIENT_EVENT_REGISTRATION_FAILURE:
+	case LWM2M_RD_CLIENT_EVENT_NETWORK_ERROR:
+		gwto.lwm2m_connection_err = true;
+		break;
+	default:
+		gwto.lwm2m_connection_err = false;
+		break;
+	}
+}
+
 static void ble_gw_dm_thread(void *arg1, void *arg2, void *arg3)
 {
 	LOG_INF("BLE Gateway Device Manager Started");
@@ -182,10 +241,21 @@ static void ble_gw_dm_thread(void *arg1, void *arg2, void *arg3)
 
 	set_state(GW_DM_STATE_WAIT_FOR_NETWORK);
 #if defined(CONFIG_LCZ_BLE_GW_DM_CONNECTION_DELAY)
-	dm_connection_delay_seconds = CONFIG_LCZ_BLE_GW_DM_CONNECTION_DELAY;
+	gwto.dm_connection_delay_seconds = CONFIG_LCZ_BLE_GW_DM_CONNECTION_DELAY;
 #else
-	dm_connection_delay_seconds = 0;
+	/* this is a placeholder for a future runtime setting */
+	gwto.dm_connection_delay_seconds = 0;
 #endif
+
+#if defined(CONFIG_LCZ_BLE_GW_DM_CONNECTION_TIMEOUT)
+	gwto.dm_connection_timeout_seconds = CONFIG_LCZ_BLE_GW_DM_CONNECTION_TIMEOUT;
+#else
+	/* this is a placeholder for a future runtime setting */
+	gwto.dm_connection_timeout_seconds = 90;
+#endif
+
+	lwm2m_event_agent.connected_callback = lwm2m_client_connected_event;
+	(void)lcz_lwm2m_client_register_event_callback(&lwm2m_event_agent);
 
 	Framework_RegisterTask(&gwto.msgTask);
 	Framework_StartTimer(&gwto.msgTask);
