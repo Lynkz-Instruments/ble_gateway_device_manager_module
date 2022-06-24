725277014b2194d9c84fe73f407671e1c892ee1b
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-06-24 09:18:33 -0500

    Memfault: Save data to file for NB-IoT
    Save Memfault data to file for NB-IoT, otherwise use HTTP POST.
    
    bug 21921

--- src/memfault_task.c
+++ src/memfault_task.c
@@ -16,13 +16,23 @@ LOG_MODULE_REGISTER(memfault_task, CONFIG_LCZ_BLE_GW_DM_LOG_LEVEL);
 #include <zephyr.h>
 #include <drivers/modem/hl7800.h>
 #include <memfault_ncs.h>
+#if defined(CONFIG_ATTR)
+#include "attr.h"
+#endif
 #include "lcz_memfault.h"
+#include "file_system_utilities.h"
 #include "memfault_task.h"
+
+/**************************************************************************************************/
+/* Local Constant, Macro and Type Definitions                                                     */
+/**************************************************************************************************/
+#define MEMFAULT_DATA_FILE_PATH CONFIG_FSU_MOUNT_POINT "/" CONFIG_LCZ_BLE_GW_DM_MEMFAULT_FILE_NAME
 
 /**************************************************************************************************/
 /* Local Data Definitions                                                                         */
 /**************************************************************************************************/
-struct k_timer report_data_timer;
+static struct k_timer report_data_timer;
+static uint8_t chunk_buf[CONFIG_LCZ_BLE_GW_DM_MEMFAULT_CHUNK_BUF_SIZE];
 
 /**************************************************************************************************/
 /* Global Data Definitions                                                                        */
@@ -44,14 +54,23 @@ static void report_data_timer_expired(struct k_timer *timer_id)
 
 static void memfault_thread(void *arg1, void *arg2, void *arg3)
 {
+	char *dev_id;
+	bool save_data;
+	size_t file_size;
+	bool has_coredump;
+	bool delete_file;
+
 	ARG_UNUSED(arg1);
 	ARG_UNUSED(arg2);
 	ARG_UNUSED(arg3);
 
 #ifdef CONFIG_MODEM_HL7800
-	char *imei = mdm_hl7800_get_imei();
-	memfault_ncs_device_id_set(imei, strlen(imei));
+	dev_id = mdm_hl7800_get_imei();
+#else
+	dev_id = attr_get_quasi_static(ATTR_ID_bluetooth_address);
 #endif
+
+	memfault_ncs_device_id_set(dev_id, strlen(dev_id));
 
 	LCZ_MEMFAULT_HTTP_INIT();
 	k_timer_init(&report_data_timer, report_data_timer_expired, NULL);
@@ -62,9 +81,36 @@ static void memfault_thread(void *arg1, void *arg2, void *arg3)
 
 	while (true) {
 		k_thread_suspend(memfault);
-		LOG_INF("Posting Memfault data...");
-		LCZ_MEMFAULT_POST_DATA();
-		LOG_INF("Memfault data sent!");
+#ifdef CONFIG_MODEM_HL7800
+#ifdef CONFIG_ATTR
+		if (attr_get_uint32(ATTR_ID_lte_rat, 0) == MDM_RAT_CAT_NB1) {
+			save_data = true;
+		} else
+#endif
+		{
+			save_data = false;
+		}
+#else
+		save_data = false;
+#endif
+		if (save_data) {
+			LOG_INF("Saving Memfault data...");
+			if (fsu_get_file_size_abs(MEMFAULT_DATA_FILE_PATH) >=
+			    CONFIG_LCZ_BLE_GW_DM_MEMFAULT_FILE_MAX_SIZE_BYTES) {
+				delete_file = true;
+			} else {
+				delete_file = false;
+			}
+			(void)lcz_memfault_save_data_to_file(MEMFAULT_DATA_FILE_PATH, chunk_buf,
+							     sizeof(chunk_buf), delete_file, true,
+							     &file_size, &has_coredump);
+			LOG_INF("Memfault data saved!");
+		} else {
+			LOG_INF("Posting Memfault data...");
+			(void)LCZ_MEMFAULT_POST_DATA_V2(chunk_buf, sizeof(chunk_buf));
+			LOG_INF("Memfault data sent!");
+		}
+
 		/* Reset timer each time data is sent */
 		k_timer_start(&report_data_timer,
 			      K_SECONDS(CONFIG_LCZ_BLE_GW_DM_MEMFAULT_REPORT_PERIOD_SECONDS),
@@ -81,5 +127,6 @@ int lcz_ble_gw_dm_memfault_post_data(void)
 	return 0;
 }
 
+/* Delay this thread start so it starts after main DM thread to ensure BLE address is ready */
 K_THREAD_DEFINE(memfault, CONFIG_LCZ_BLE_GW_DM_MEMFAULT_THREAD_STACK_SIZE, memfault_thread, NULL,
-		NULL, NULL, K_PRIO_PREEMPT(CONFIG_LCZ_BLE_GW_DM_MEMFAULT_THREAD_PRIORITY), 0, 0);
+		NULL, NULL, K_PRIO_PREEMPT(CONFIG_LCZ_BLE_GW_DM_MEMFAULT_THREAD_PRIORITY), 0, 1000);
