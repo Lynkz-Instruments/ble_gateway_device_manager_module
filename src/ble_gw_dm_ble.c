17b9fbe3a8dbb69fa9c94ee6c8461da5b3e8f595
Cedric <cedric@lynkz.ca>
2023-08-10 10:04:43 -0400

    Added ADV to Mg100

--- src/ble_gw_dm_ble.c
+++ src/ble_gw_dm_ble.c
@@ -15,15 +15,40 @@ LOG_MODULE_REGISTER(ble_gw_dm_ble, CONFIG_LCZ_BLE_GW_DM_LOG_LEVEL);
 /**************************************************************************************************/
 #include <zephyr/init.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/settings/settings.h>
 #include <attr.h>
 
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+
 #include "ble_gw_dm_ble.h"
+
+static struct k_work advertise_work;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+		      0x84, 0xaa, 0x60, 0x74, 0x52, 0x8a, 0x8b, 0x86,
+		      0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
+};
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME)
+};
 
 /**************************************************************************************************/
 /* Local Function Prototypes                                                                      */
 /**************************************************************************************************/
 static int ble_gw_dm_device_ble_addr_init(const struct device *device);
+static void bt_ready(int err);
+static void advertise(struct k_work *work);
+static void connected(struct bt_conn *conn, uint8_t err);
+static void disconnected(struct bt_conn *conn, uint8_t reason);
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
 
 /**************************************************************************************************/
 /* Global Function Definitions                                                                    */
@@ -50,8 +75,11 @@ static int ble_gw_dm_device_ble_addr_init(const struct device *device)
 #else
 	size = BT_ADDR_LE_STR_LEN;
 #endif
-
-	(void)bt_enable(NULL);
+	//Register SMP
+	bt_conn_cb_register(&conn_callbacks);
+	k_work_init(&advertise_work, advertise);
+	/* Enable Bluetooth. */
+	(void)bt_enable(bt_ready);
 
 	if (IS_ENABLED(CONFIG_BT_SETTINGS)) {
 		load_status = settings_load();
@@ -90,3 +118,54 @@ static int ble_gw_dm_device_ble_addr_init(const struct device *device)
 
 	return ret;
 }
+
+static void advertise(struct k_work *work)
+{
+	int rc;
+
+	rc = bt_le_adv_stop();
+	if (rc) {
+		LOG_WRN("Advertising failed to stop (rc %d)", rc);
+	}
+
+	rc = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (rc) {
+		LOG_ERR("Advertising failed to start (rc %d)", rc);
+		return;
+	}
+
+	LOG_INF("Advertising successfully started");
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		LOG_ERR("Connection failed (err 0x%02x)", err);
+	} else {
+		LOG_INF("Connected");
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	LOG_INF("Disconnected (reason 0x%02x)", reason);
+	k_work_submit(&advertise_work);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_ready(int err)
+{
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)", err);
+		return;
+	}
+
+	LOG_INF("Bluetooth initialized");
+
+	k_work_submit(&advertise_work);
+}
+
